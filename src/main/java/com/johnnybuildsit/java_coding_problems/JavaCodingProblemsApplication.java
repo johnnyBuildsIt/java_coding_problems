f231023307e90e8a66b33b069ec589c587f818ac
johnnybuildsit <johnnypr@udel.edu>
2020-11-15 14:12:07 -0500

    initial commit

---
+++ src/main/java/com/johnnybuildsit/java_coding_problems/JavaCodingProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.johnnybuildsit.java_coding_problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodingProblemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johnnybuildsit/java_coding_problems/JavaCodingProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnnybuildsit.java_coding_problems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
