950b4d10edfdcc2b6e5799181e1cd71890f29424
johnnybuildsit <johnnypr@udel.edu>
2021-02-19 16:30:05 -0500

    formatting

--- src/main/java/com/johnnybuildsit/java_coding_problems/slidingWindows/SlidingWindows.java
+++ src/main/java/com/johnnybuildsit/java_coding_problems/slidingWindows/SlidingWindows.java
@@ -10,15 +10,17 @@ public class SlidingWindows {
     // time complexity O(input * windowSize)
     public int maxSumSubArraySizeKBruteForce(final int windowSize, final int[] input) {
         int maxSum = 0;
-        for(int i = 0; i < input.length - windowSize; i++){
+
+        for(int i = 0; i < input.length - windowSize; i++) {
             int curSum = 0;
-            for(int j = i; j < i + windowSize; j++){
+            for(int j = i; j < i + windowSize; j++) {
                 curSum += input[j];
             }
-            if(curSum > maxSum){
+            if(curSum > maxSum) {
                 maxSum = curSum;
             }
         }
+
         return maxSum;
     }
 
@@ -27,14 +29,17 @@ public class SlidingWindows {
         int maxSum = 0;
         int curSum = 0;
         int windowStart = 0;
-        for(int windowEnd = 0; windowEnd < input.length; windowEnd++){
+
+        for(int windowEnd = 0; windowEnd < input.length; windowEnd++) {
             curSum += input[windowEnd];
-            if(windowEnd >= windowSize - 1){//if windowSize = 3, want index 0,1,2
+
+            if(windowEnd >= windowSize - 1) {//if windowSize = 3, want index 0,1,2
                 maxSum = Math.max(curSum, maxSum);
                 curSum -= input[windowStart];
                 windowStart++;
             }
         }
+
         return maxSum;
     }
 
@@ -44,9 +49,11 @@ public class SlidingWindows {
      */
     public int smallestSubArrayWithGivenSumBruteForce(final int targetSum, final int[] input) {
         int smallestWindow = Integer.MAX_VALUE;
+
         for(int i = 0; i < input.length; i++) {
             int curWindow = 0;
             int curSum = 0;
+
             for(int j = i; j < input.length; j++){
                 curWindow += 1;
                 curSum += input[j];
@@ -57,8 +64,8 @@ public class SlidingWindows {
                     break;
                 }
             }
-
         }
+
         return smallestWindow;
     }
 
@@ -69,7 +76,7 @@ public class SlidingWindows {
         int smallestWindow = Integer.MAX_VALUE;
         int windowStart = 0, windowEnd = 0;
 
-        while(windowEnd < input.length){
+        while(windowEnd < input.length) {
             // increase window
             while(curSum < targetSum) {
                 windowEnd++;
@@ -79,7 +86,6 @@ public class SlidingWindows {
                 } else {
                     break;
                 }
-
             }
 
             smallestWindow = Math.min(curWindow, smallestWindow);
@@ -88,7 +94,8 @@ public class SlidingWindows {
                 curSum -= input[windowStart];
                 windowStart++;
                 curWindow--;
-                if (curSum == targetSum){
+
+                if (curSum == targetSum) {
                     smallestWindow = Math.min(curWindow, smallestWindow);
                 }
             }
@@ -98,9 +105,11 @@ public class SlidingWindows {
 
     public int longestSubstringWithDistinctChars(final String input, final int lettersAllowed) {
         int maxSubstringLen = 0;
+
         for(int i = 0; i < input.length(); i++) {
             final HashSet<Character> curChars = new HashSet<>();
             int curSubstringLen = 0;
+
             for(int j = i; j < input.length(); j++) {
                 if(!curChars.contains(input.charAt(j)) && curChars.size() == lettersAllowed){
                     maxSubstringLen = Math.max(maxSubstringLen, curSubstringLen);
